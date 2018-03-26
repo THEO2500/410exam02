21dabe4fea011996e77478933bbc4fdc5cc8bc97
THEO2500 <worldpeace871219@gmail.com>
2018-03-26 19:18:20 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+	Scanner in= new Scanner(System.in);
+	float a,b;
+	a=in.nextFloat();
+	b=(a*9/5)+32;
+	System.out.printf("%f\n",b);
+
+    }
+}
